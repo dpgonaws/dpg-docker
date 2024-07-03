bf64038c9da3691ba39672f2fc4498b9a376952a
Amit Kumar <amitvashist7@gmail.com>
2024-07-03 13:59:09 +0000

    First commit

---
+++ esignet-init/EsignetEval.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+NS=esignet
+
+LOCAL_ESIGNET_FILE_NAME=esignet.properties
+CHANGED_ESIGNET_FILE_NAME=esignet-local.properties
+
+export ESIGNET_HOST_VALUE=$(kubectl get cm global -n esignet -o jsonpath={.data.mosip-esignet-host})
+export API_INTERNAL_VALUE=$(kubectl get cm global -n esignet -o jsonpath={.data.mosip-api-internal-host})
+export KEYCLOAK_EXTERNAL_URL_VALUE=$(kubectl get cm global -n esignet -o jsonpath={.data.mosip-iam-external-host})
+export SOFTHSM_SECURITY_PIN_VALUE=$(kubectl get secrets softhsm -n softhsm -o jsonpath={.data.security-pin} | base64 --decode)
+export AWS_MANAGED_KAFKA_URL=$AWS_KAFKA_ARN
+export DB_HOSTNAME_VALUE=$DB_HOSTNAME
+export DB_PORT_VALUE=$DB_PORT
+export DB_USERNAME_VALUE=$DB_USERNAME
+export DB_PASSWORD_VALUE=$DB_PASSWORD
+
+echo "setting namespace\n"
+echo $NS
+
+echo "ESIGNET_HOST_VALUE\n"
+echo $ESIGNET_HOST_VALUE
+
+echo "API_INTERNAL_VALUE\n"
+echo $API_INTERNAL_VALUE
+
+echo "KEYCLOAK_EXTERNAL_URL_VALUE\n"
+echo $KEYCLOAK_EXTERNAL_URL_VALUE
+
+echo "SOFTHSM_SECURITY_PIN_VALUE\n"
+echo $SOFTHSM_SECURITY_PIN_VALUE
+
+echo "AWS_MANAGED_KAFKA_URL\n"
+echo $AWS_MANAGED_KAFKA_URL
+
+echo "DB_HOSTNAME_VALUE\n"
+echo $DB_HOSTNAME_VALUE
+
+echo "DB_PORT_VALUE\n"
+echo $DB_PORT_VALUE
+
+echo "DB_USERNAME_VALUE\n"
+echo $DB_USERNAME_VALUE
+
+echo "DB_PASSWORD_VALUE\n"
+echo $DB_PASSWORD_VALUE
+
+
+
+envsubst < $LOCAL_ESIGNET_FILE_NAME  > $CHANGED_ESIGNET_FILE_NAME
+
+echo "properties file updated....\n"
+
+kubectl create configmap esignet-local-properties -n $NS  --from-file=$CHANGED_ESIGNET_FILE_NAME
+
+
+
+kubectl -n artifactory get configmap artifactory-share -o yaml | sed "s/namespace: artifactory/namespace: $NS/g" |  kubectl -n $NS create -f - 
+kubectl -n softhsm get configmap softhsm-share -o yaml | sed "s/namespace: softhsm/namespace: $NS/g" | sed "s/name: softhsm-share/name: softhsm-esignet-share/g"|  kubectl -n $NS create -f -
+
+
+echo "esignet init setup completed"
---
+++ vault-init-inji/VaultInit.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+
+# Read options and corresponding values
+#while getopts "n:v:" option; do
+#   case "$option" in
+#       n) NAMESPACE=${OPTARG};;
+#       v) VAULT_NAME=${OPTARG};;
+#   esac
+#done
+
+#NAMESPACE="anvault"
+#VAULT_NAME="anvault"
+
+
+
+echo $NAMESPACE
+echo $VAULT_NAME
+
+#echo -e "\nKubectl namespace creation\n"
+#kubectl create namespace $NAMESPACE
+
+
+#echo -e "\nVault helm installation\n"
+
+#helm install $VAULT_NAME hashicorp/vault --version 0.24.0 --values helm-vault-raft-values.yml -n $NAMESPACE --wait --timeout 120s
+
+
+#echo -e "\nChecking vault pod status\n"
+#helm status $VAULT_NAME -n $NAMESPACE
+
+
+# Wait until the deployment is complete
+#until helm status "$VAULT_NAME" -n $NAMESPACE  | grep "STATUS: deployed"; do
+#  echo "Waiting for deployment to complete..."
+#  sleep 1
+#done
+# The deployment is complete
+#echo "Deployment complete!"
+
+
+
+#until false
+#do
+ # If there are other pods running in the namespace wll thsi help?
+ #   if [[ $(kubectl get pods -n $NAMESPACE | grep "Running" | wc -l) -eq 2 ]]
+ #   then
+
+ #       echo -e "Container running succesfully"
+ #       break
+ #   else
+
+#        echo "Waiting for container to be running"
+#        sleep 2
+#        continue
+#    fi
+#done
+
+# ####################### VAULT CONFIGURATION AFTER INSTALL ################################
+
+
+echo -e "\nChecking vault status\n"
+kubectl exec $VAULT_NAME-0 -n $NAMESPACE -- vault status
+
+sleep 10
+
+echo -e "Vault is getting Initialized\n\n"
+kubectl exec $VAULT_NAME-0 -n $NAMESPACE -- vault operator init -key-shares=1 -key-threshold=1 -format=json > vault-keys.json
+#echo -e "VI. Show unsealkeys"
+
+sleep 30
+
+#cat vault-keys.json | jq -r ".unseal_keys_b64[]"
+echo -e "\nVault unseal key is getting set\n"
+
+VAULT_UNSEAL_KEY=$(cat vault-keys.json | jq -r ".unseal_keys_b64[]")
+echo -e "\nVault unseal key is set. This can be checked using \n echo '$'VAULT_UNSEAL_KEY\n"
+
+kubectl create secret generic vault-unseal-key --from-literal=VAULT_UNSEAL_KEY=$VAULT_UNSEAL_KEY -n $NAMESPACE
+
+
+echo -e "\nExecuting Vault Unseal operation\n"
+kubectl exec $VAULT_NAME-0 -n $NAMESPACE -- vault operator unseal $VAULT_UNSEAL_KEY
+#echo "***** vault unseal status"
+
+#kubectl exec $VAULT_NAME-0 -n $NAMESPACE -- vault status
+echo -e "\nExtract vault root token\n\n"
+
+CLUSTER_ROOT_TOKEN=$(cat vault-keys.json | jq -r ".root_token")
+
+kubectl create secret generic vault-cluster-token --from-literal=ROOT_TOKEN=$CLUSTER_ROOT_TOKEN -n $NAMESPACE
+
+echo -e "\nVault Root token is set. This can be checked using \n echo '$'CLUSTER_ROOT_TOKEN"
+echo -e "\nAuthenticate vaultt\n"
+
+kubectl exec $VAULT_NAME-0 -n $NAMESPACE -- vault login $CLUSTER_ROOT_TOKEN
+#echo "V. Vault Init"
+
+#kubectl exec --stdin=true --tty=true $VAULT_NAME-0 -n $NAMESPACE -- /bin/sh
+echo -e "\nSet vault enable kubernetes\n"
+
+kubectl exec $VAULT_NAME-0 -n $NAMESPACE -- vault auth enable kubernetes
+echo -e "\nvault set secret path\n"
+
+kubectl exec $VAULT_NAME-0 -n $NAMESPACE -- vault secrets enable -path=kv kv-v2
+
+echo -e "\nVault setup completed successfully!!!"
+
